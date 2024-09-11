b7e6746cbdee887fa7f964e952a6b05342aee58b
mauriciocursos <mauriciocursos2024@gmail.com>
2024-09-11 10:37:36 -0300

    Criação do banco de dados e atualização main com funções de banco

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import flet as ft
+import sqlite3
 
 class ToDo:
     def __init__(self, page: ft.Page):
@@ -9,15 +10,81 @@ class ToDo:
         self.page.window_resizable = True
         self.page.window_always_on_top = True
         self.page.title = 'Tarefas App'
+        self.task = ''
+        self.view = 'all'
+        self.db_execute('CREATE TABLE IF NOT EXISTS tasks(name, status)')
+        self.results = self.db_execute('SELECT * FROM tasks')
         self.main_page()
 
+    def db_execute(self, query, params = []):
+        with sqlite3.connect('database.db') as con:
+            cur = con.cursor()
+            cur.execute(query, params)
+            con.commit()
+            return cur.fetchall()
+        
+    def checked(self, e):
+        is_checked = e.control.value
+        label = e.control.label
+
+        if is_checked:
+            self.db_execute('UPDATE tasks SET status = "Completa" WHERE name = ?', params=[label])
+        else:
+            self.db_execute('UPDATE tasks SET status = "Incompleta" WHERE name = ?', params=[label])
+        
+        if self.view == 'all':
+            self.results = self.db_execute('SELECT * FROM tasks')
+        else:
+            self.results = self.db_execute('SELECT * FROM tasks WHERE status = ?', params=[self.view])
+
+        self.update_task_list()
+
+    def tasks_container(self):
+        return ft.Container(
+            
+            content = ft.Column(
+                controls = [
+                    ft.Checkbox(label=res[0],
+                                on_change = self.checked,
+                                value=True if res[1] == 'Completa' else False
+                                ) for res in self.results if res
+                ],
+            )
+        )
+    
+    def set_value(self, e):
+        self.task = e.control.value
+    
+    def add(self, e, input_task):
+        name = self.task
+        status = 'incompleta'
+
+        if name:
+            self.db_execute(query='INSERT INTO tasks VALUES(?,?)', params=[name, status])
+            input_task.value = ''
+            self.results = self.db_execute('SELECT * FROM tasks')
+            self.update_task_list()
+
+    def update_task_list(self):
+        tasks = self.tasks_container()
+        self.page.controls.pop()
+        self.page.add(tasks)
+        self.page.update()
+
     def main_page(self):
-        input_task = ft.TextField(hint_text = 'Digite aqui uma tarefa', expand=True)
+        input_task = ft.TextField(
+            hint_text = 'Digite aqui uma tarefa', 
+            expand=True, 
+            on_change=self.set_value
+            )
 
         input_bar = ft.Row(
             controls=[
                 input_task,
-                ft.FloatingActionButton(icon=ft.icons.ADD)
+                ft.FloatingActionButton(
+                    icon=ft.icons.ADD,
+                    on_click=lambda e: self.add(e, input_task)
+                    )
             ]
         )
 
@@ -30,7 +97,9 @@ class ToDo:
             ]
         )
 
-        self.page.add(input_bar, tabs)
+        tasks = self.tasks_container()
+
+        self.page.add(input_bar, tabs, tasks)
 
 
 ft.app(target = ToDo)
